c176eaded9b8a7e2675a1ef9fe600daede06be5c
Harry <harald.innetzberger@web.de>
2021-08-05 16:44:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ fastify.register(require('point-of-view'), {
 });
 fastify.register(require('./routes/items'));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // run the server
 const start = async () => {
